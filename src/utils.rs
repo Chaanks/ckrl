c7a9a8ad7fdf89558b46d09dcca7b4f546f50b5f
Chaanks <jarod8405@gmail.com>
2020-01-30 21:16:00 +0100

    add logger

---
+++ src/utils.rs
@@ -0,0 +1,43 @@
+use std::io::Write;
+
+
+pub fn start_logger() {
+    env_logger::Builder::new()
+        .format(|buf, record| {
+            writeln!(buf,
+                "{} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, log::LevelFilter::Info)
+        .init();
+}
+
+
+#[cfg(test)]
+mod tests { 
+
+    use super::start_logger;
+    use log::{debug, info, warn, error};
+
+    #[test]
+    fn make_logger() {
+        start_logger();
+
+        warn!("warn");
+        info!("info");
+        debug!("debug");
+    
+        info!("such information");
+        info!("such information");
+        warn!("o_O");
+        warn!("o_O");
+        error!("boom");
+        error!("boom");
+        debug!("deboogging");
+        debug!("deboogging");
+    }
+
+}
