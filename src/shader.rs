5b4d478586d04304f09f797ee818a63d53f0939c
Chaanks <jarod8405@gmail.com>
2020-02-03 22:04:29 +0100

    complete shader implementation

--- src/shader.rs
+++ src/shader.rs
@@ -18,19 +18,39 @@ pub fn new_program(
     // compile shaders from strings
     unsafe {
         // vertex shader
-        let vertex = gl.create_shader(glow::VERTEX_SHADER)?;
-        gl.shader_source(vertex, &vertex_code);
-        gl.compile_shader(vertex);
-        if !gl.get_shader_compile_status(vertex) {
+        let vertex_id = gl.create_shader(glow::VERTEX_SHADER)?;
+        gl.shader_source(vertex_id, &vertex_code);
+        gl.compile_shader(vertex_id);
+        if !gl.get_shader_compile_status(vertex_id) {
             error!("Failed to compile vertex shader");
-            return Err(failure::err_msg(gl.get_shader_info_log(vertex)).into());
+            return Err(failure::err_msg(gl.get_shader_info_log(vertex_id)).into());
         }
 
         // fragment shader
+        let fragment_id = gl.create_shader(glow::FRAGMENT_SHADER)?;
+        gl.shader_source(fragment_id, &fragment_code);
+        gl.compile_shader(fragment_id);
+        if !gl.get_shader_compile_status(fragment_id) {
+            error!("Failed to compile fragment shader");
+            return Err(failure::err_msg(gl.get_shader_info_log(fragment_id)).into());
+        }
+
+        // shader program
+        let program_id = gl.create_program()?;
+        gl.attach_shader(program_id, vertex_id);
+        gl.attach_shader(program_id, fragment_id);
+        gl.link_program(program_id);
+        if !gl.get_program_link_status(program_id) {
+            error!("Failed to link program");
+            return Err(failure::err_msg(gl.get_program_info_log(program_id)).into());
+        }
+
+        gl.delete_shader(vertex_id);
+        gl.delete_shader(fragment_id);
         
+        Ok(RawProgram { id:program_id })
     }
 
-    Ok(RawProgram { id:32 })
 
 }
     
--- src/shader_string.rs
+++ src/shader_string.rs
@@ -6,3 +6,12 @@ void main()
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
 "#;
+
+pub static FRAGMENT_SHADER: &str = r#"#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+}
+"#;
